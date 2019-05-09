6f725c71253209020e0ccf20b39835dc4de40a1d
Erik Cui <erikcui9020@gmail.com>
2019-05-09 14:07:24 -0700

    updated with main functions and helper functions
    progress is still underway

--- caption_meme_bot.py
+++ caption_meme_bot.py
@@ -1,20 +1,41 @@
 import praw
 import numpy as np 
 from PIL import Image
+import json
 
-reddit = praw.Reddit('caption')
+def main():
+    global json_data
+    with open("replied_comments.json", "r") as f:
+        json_data = json.load(f)
 
-subreddit = reddit.subreddit("caption_meme_bot_test")
+    reddit = praw.Reddit("caption")
+    subreddit = reddit.subreddit("caption_meme_bot_test")
+    match_keyword = "!caption"
 
-for submission in subreddit.hot(limit=5):
+    for submission in subreddit.hot(limit=5):
+        print_submission(submission)
+        submission.comments.replace_more(limit=0)
+        # the list method converts ALL comments, not just TOP-level, to a list structure
+        for comment in submission.comments.list():
+            if match_keyword in str(comment.body) and comment.id not in json_data["replied_comments"]:
+                reply_to(comment)
+                store_comment_id(comment.id)
+
+def reply_to(comment):
+    parent_comment = comment.parent()
+    print(parent_comment.body)
+    #comment.reply("I have replied to this comment")
+
+def store_comment_id(id):
+    json_data["replied_comments"].append(id)
+    with open("replied_comments.json", "w") as f:
+       json.dump(json_data, f, ensure_ascii=False)
+
+def print_submission(submission):
     print("Title: ", submission.title)
     print("Text: ", submission.selftext)
     print("Score: ", submission.score)
     print("---------------------------------\n")
 
-    submission.comments.replace_more(limit=0)
-    # the list method converts ALL comments, not just TOP-level, to a list structure
-    for comment in submission.comments.list():
-        print(comment.body)
-    
-    # you can access a reply from comment.replies and iterating through it
+if __name__ == "__main__":
+    main()
--- notification_bot.py
+++ notification_bot.py
@@ -1,19 +1,23 @@
 import praw
 
-reddit = praw.Reddit('notification')
+def main():
+    reddit = praw.Reddit("notification")
 
-# TODO: create a separate bot account for testing this bot
-subreddit = reddit.subreddit("caption_meme_bot_test")
+    # TODO: create a separate bot account for testing this bot
+    subreddit = reddit.subreddit("caption_meme_bot_test")
 
-for submission in subreddit.hot(limit=5):
-    print("Title: ", submission.title)
-    print("Text: ", submission.selftext)
-    print("Score: ", submission.score)
-    print("---------------------------------\n")
+    for submission in subreddit.hot(limit=5):
+        print("Title: ", submission.title)
+        print("Text: ", submission.selftext)
+        print("Score: ", submission.score)
+        print("---------------------------------\n")
 
-    submission.comments.replace_more(limit=0)
-    # the list method converts ALL comments, not just TOP-level, to a list structure
-    for comment in submission.comments.list():
-        print(comment.body)
-    
-    # you can access a reply from comment.replies and iterating through it
+        submission.comments.replace_more(limit=0)
+        # the list method converts ALL comments, not just TOP-level, to a list structure
+        for comment in submission.comments.list():
+            print(comment.body)
+        
+        # you can access a reply from comment.replies and iterating through it
+
+if __name__ == "__main__":
+    main()
