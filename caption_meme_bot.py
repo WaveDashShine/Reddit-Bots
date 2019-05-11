76daecd1bb4d3ae752ff426c6cc4d51b0f60ea7b
Erik Cui <erikcui9020@gmail.com>
2019-05-10 23:04:05 -0700

    researched image replies and added TODOs

--- caption_meme_bot.py
+++ caption_meme_bot.py
@@ -2,8 +2,14 @@ import praw
 import numpy as np 
 from PIL import Image
 import json
+import requests
+from io import BytesIO
+import re
 
 def main():
+    global regex_image_extension
+    regex_image_extension = re.compile(".*\.(jpg|png|jpeg)")
+
     global json_data
     with open("replied_comments.json", "r") as f:
         json_data = json.load(f)
@@ -35,7 +41,22 @@ def print_submission(submission):
     print("Title: ", submission.title)
     print("Text: ", submission.selftext)
     print("Score: ", submission.score)
+    print("URL: ", submission.url)
     print("---------------------------------\n")
+
+def generate_image_macro(url_image):
+    if regex_image_extension.match(url_image, re.IGNORECASE):
+        response = requests.get(url_image)
+        img = Image.open(BytesIO(response.content))
+
+# TODO: submit the image on the private subreddit, 
+# grab the url of the submitted image,
+# then link to it in the comments,
+# remember to append the comment reply with some bot disclaimer 
+# https://praw.readthedocs.io/en/latest/code_overview/models/subreddit.html#praw.models.Subreddit.submit_image
+
+# TODO: use submission stream instead of scanning through submission comment
+# https://praw.readthedocs.io/en/latest/code_overview/models/subreddit.html#praw.models.Subreddit.stream
 
 if __name__ == "__main__":
     main()
